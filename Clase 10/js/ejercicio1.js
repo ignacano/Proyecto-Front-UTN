931b3197363b7d2ead77a7264d82a9796ca7234d
Ignacio <ignacioalfredocano@gmail.com>
2022-10-20 23:17:28 -0300

    Agrego archivos clase 10

---
+++ Clase 10/js/ejercicio1.js
@@ -0,0 +1,33 @@
+
+// let nombre = 'Ignacio';
+// let apellido = 'Cano';
+// let edad = 32;
+// let tenerMascotas = true
+
+// console.log (nombre, apellido, edad, tenerMascotas)
+
+// let numeroA = 12;
+// let numeroB = 3;
+// let resultadoSuma = numeroA + numeroB
+// console.log (resultadoSuma)
+
+// let resultadoMul = numeroA * numeroB
+// console.log (resultadoMul)
+
+// let textoA = 'Hola';
+// let textoB = 'Mundo';
+// const BLANCO = ' ';
+
+
+
+// let resultadoA = textoA+BLANCO+textoB; //puedo guardar un resultado en una variable, y usar la suma para concatenar
+// console.log (resultadoA)
+
+// let entrada = prompt ('Ingrese su nombre')
+// let salida = "Hola "+entrada;
+// alert(salida);
+
+let numero =parseInt(prompt("Ingrese un numero"))
+const numeroB = 30;
+let salida = numero+numeroB;
+alert(salida);
---
+++ Clase 10/js/main.js
@@ -0,0 +1,17 @@
+console.log ('hola mundo desde archivo externo')
+
+var nomb = 'Harry';
+
+console.log (nomb);
+//String
+let nombre = 'Harry';
+const NOMBRE = 'Harry';
+
+console.log (nombre);
+//Numero
+let edad = 20;
+const NUM = 10.2;
+
+console.log (edad);
+
+
